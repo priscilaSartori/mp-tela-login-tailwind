c26bbca69af84ee8abae64f7b26e61208af76832
Priscila <priscilasartori2@gmail.com>
2023-05-25 23:33:20 -0300

    feat: Configuração inicial

---
+++ tela-login/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Route, Routes } from 'react-router-dom';
+
+class App extends React.Component {
+  render() {
+    return (
+        <Routes>
+          
+        </Routes>
+    );
+  }
+}
+
+export default App;
---
+++ tela-login/tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js,jsx,ts,tsx}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ["DM sans", "sans-serif"],
+      }
+    },
+  },
+  plugins: [
+    require('@tailwindcss/forms'),
+  ],
+}
+
